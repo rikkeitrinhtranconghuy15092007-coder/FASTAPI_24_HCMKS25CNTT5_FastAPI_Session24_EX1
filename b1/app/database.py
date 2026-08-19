cad02c4ab3399c0c3b27965fccb1703ab0de0582
Trịnh Trần Công Huy <rikkei.trinhtranconghuy15092007@gmail.com>
2026-08-19 07:55:55 +0700

    Add database Python script

---
+++ b1/app/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine 
+from sqlalchemy.orm import sessionmaker,declarative_base 
+
+DATABASE_URL = "mysql+pymysql://root:123456@localhost:3306/demo"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autoflush=False,autocommit=False,bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db 
+    finally:
+        db.close()
+        
